5411db03d68f04021b438c56eed0cb5445e2f739
Anishgupta1234 <anish1775.be23@chitkara.edu.in>
2026-07-03 12:37:07 +0530

    COmmiting code for linked list for add in first and add at end

---
+++ LinkedList.java
@@ -0,0 +1,68 @@
+import java.util.*;
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+public class LinkedList {
+    Node head;
+    void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+    void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+    void display() {
+        if (head == null) {
+            System.out.println("Linked List is Empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data);
+            if (temp.next != null) {
+                System.out.print(" -> ");
+            }
+            temp = temp.next;
+        }
+        System.out.println(" -> NULL");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedList list = new LinkedList();
+        System.out.print("Enter number of elements to insert at beginning: ");
+        int n1 = sc.nextInt();
+        System.out.println("Enter " + n1 + " elements:");
+        for (int i = 0; i < n1; i++) {
+            int data = sc.nextInt();
+            list.insertAtBeginning(data);
+        }
+        System.out.println("\nLinked List after Insertion at Beginning:");
+        list.display();
+        System.out.print("\nEnter number of elements to insert at end: ");
+        int n2 = sc.nextInt();
+        System.out.println("Enter " + n2 + " elements:");
+        for (int i = 0; i < n2; i++) {
+            int data = sc.nextInt();
+            list.insertAtEnd(data);
+        }
+        System.out.println("\nLinked List after Insertion at End:");
+        list.display();
+        sc.close();
+    }
+}
